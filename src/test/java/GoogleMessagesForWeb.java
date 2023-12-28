53515d7305e9b2b7d908d5d52560cb579207e3d8
npecic <npecic.ia@gmail.com>
2023-12-28 08:35:55 -0500

    Initial commit

---
+++ src/test/java/GoogleMessagesForWeb.java
@@ -0,0 +1,46 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class GoogleMessagesForWeb {
+
+    WebDriver driver;
+
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver","src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+    @AfterMethod
+    public void tearDown (){
+        driver.quit();
+
+    }
+
+    @Test
+    public void sendMessages() throws InterruptedException {
+
+        String[] numbers = {"", "", ""};
+        driver.get("https://messages.google.com/web/authentication");
+        Thread.sleep(20000);
+
+        for (int i = 0; i <numbers.length;i++){
+
+            driver.findElement(By.xpath("//div[contains(text(),'Start chat')]/../..")).click();
+            driver.findElement(By.xpath("//input[@placeholder='Type a name, phone number, or email']")).sendKeys(numbers[i]);
+            driver.findElement(By.xpath("//input[@placeholder='Type a name, phone number, or email']")).sendKeys(Keys.ENTER);
+
+            driver.findElement(By.xpath("//textarea")).sendKeys("Cao");
+            driver.findElement(By.xpath("//textarea")).sendKeys(Keys.ENTER);
+        }
+    }
+}
